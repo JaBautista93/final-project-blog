f1470726fce7b530ad3ef6f9896771606d4ba388
John-Anthony <41270598+JaBautista93@users.noreply.github.com>
2019-01-26 14:03:44 -0600

    update

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -30,7 +30,7 @@ module.exports = {
   update: function(req, res) {
     db.Blog
       .updateOne({ _id: req.params.id }, 
-        { $push: { response: req.body.response } },) 
+        { $push: { response: req.body.response } },)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
       },
