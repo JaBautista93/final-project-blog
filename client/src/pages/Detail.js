e5c7e97c71586c4c8468a9491e780cc5c9201946
John-Anthony <41270598+JaBautista93@users.noreply.github.com>
2019-01-28 21:35:45 -0600

    fixed one item

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
-import { TextArea, FormBtn } from "../components/Form";
+import { TextArea, FormBtn, } from "../components/Form";
+import DeleteBtn from "../components/DeleteBtn";
+import { List, ListItem, } from "../components/List";
+
+
+
 
 class Detail extends Component {
   state = {
@@ -12,26 +17,25 @@ class Detail extends Component {
   // When this component mounts, grab the book with the _id of this.props.match.params.id
   // e.g. localhost:3000/books/599dcb67f0f16317844583fc
   componentDidMount() {
+    this.loadBlog();
+  };
+  
+  loadBlog = () => {
     API.getBlog(this.props.match.params.id)
-      .then(res => this.setState({ blog: res.data, }))
+      .then(res => {
+        console.log('DATA: ', res.data);
+        this.setState({ blog: res.data });
+      })
       .catch(err => console.log(err));
   };
   
-  loadBlogs = () => {
-    API.getBlogs()
-      .then(res =>
-        this.setState({ blogs: res.data, topic: "", author: "", synopsis: "" })
-      )
-      .catch(err => console.log(err));
-  };
-  
-  updateBlogs = () => {
-    API.updateBlog()
-      .then(res =>
-        this.setState({ blogs: res.data, topic: "", author: "", synopsis: "", response: "" })
-      )
-      .catch(err => console.log(err));
-  };
+  // updateBlogs = () => {
+  //   API.updateBlog()
+  //     .then(res =>
+  //       this.setState({ blogs: res.data, topic: "", author: "", synopsis: "", response: "" })
+  //     )
+  //     .catch(err => console.log(err));
+  // };
 
   handleResponseChange = event => {
     const { value } = event.target;
@@ -45,19 +49,27 @@ class Detail extends Component {
     console.log(this.state);
     if (this.state.blog.response) {
       API.updateBlog(this.state.blog._id, this.state.blog)
-        .then(res => this.loadBlogs())
+        .then(res => this.loadBlog())
         .catch(err => console.log(err));
     }
   };
 
+  deleteBlog = id => {
+    API.deleteBlog(id)
+      .then(res => this.loadBlog())
+      .catch(err => console.log(err));
+  };
+
   render() {
+    const blog = this.state.blog;
+    const { topic, author, synopsis, response, _id} = blog;
     return (
       <Container fluid>
         <Row>
           <Col size="md-12">
             <Jumbotron>
               <h1>
-                {this.state.blog.topic} by {this.state.blog.author}
+                {topic} by {author}
               </h1>
             </Jumbotron>
           </Col>
@@ -67,11 +79,23 @@ class Detail extends Component {
             <article>
               <h1>Synopsis</h1>
               <p>
-                {this.state.blog.synopsis}
+                {synopsis}
               </p>
+              <p>
+                <List>
+                  <ListItem key={_id}>
+                    <Link to={"/blogs/" + _id}>
+                      <strong>
+                        {response}
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => this.deleteBlog(_id)} />
+                  </ListItem>
+                </List>
+                </p>
               <form>
               <TextArea
-                value={this.state.blog.response}
+                value={response}
                 onChange={this.handleResponseChange}
                 name="response"
                 placeholder="Provide Response"
